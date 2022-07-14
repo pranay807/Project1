1b261e2086595c76c9d2c31f904a227b291e13ee
SakshiPingle <89467228+SakshiPingle@users.noreply.github.com>
2022-07-14 16:20:48 +0530

    Update Hello.java

--- ProjectNew/src/Hello.java
+++ ProjectNew/src/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Welcome to India");
+		System.out.println("Hello pran,saki,ruchi");
+		
 	}
 
 }
